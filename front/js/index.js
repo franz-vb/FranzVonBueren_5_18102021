087e95d5726826f2768384a486ed40f0bd0c5335
franz von büren <fz.v.bueren@gmail.com>
2021-10-27 16:41:32 +0200

    add product

---
+++ front/js/index.js
@@ -0,0 +1,75 @@
+// const test = document.querySelector("#test");
+// const items = document.querySelector(".items");
+
+// function createProduct(data, i) {
+
+//     /* creation parent a */
+
+//     const a = document.createElement("a");
+//     items.appendChild(a);
+
+//     /* creation parent article */
+
+//     const article = document.createElement("article");
+//     a.appendChild(article);
+
+//     /* creation enfants article */
+
+//     const imgArticle = document.createElement("img");
+//     const titreArticle = document.createElement("h3");
+//     const descriptionArticle = document.createElement("p");
+
+//     article.appendChild(imgArticle);
+//     article.appendChild(titreArticle);
+//     article.appendChild(descriptionArticle);
+
+//     imgArticle.src = data[i].imageUrl;
+//     imgArticle.alt = "photo du meuble";
+
+//     titreArticle.className = "productName";
+//     titreArticle.textContent = data[i].name;
+
+//     descriptionArticle.className = "productDescription";
+//     descriptionArticle.textContent = data[i].description;
+// }
+
+// function getDataFromApi() {
+//   fetch("http://localhost:3000/api/products/")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       //for (let i = 0; i < data.length; i++) {
+//       //for(product of data) {
+//       data.map((product) => {
+//         //createProduct(data, i);
+//         document.querySelector(".items").innerHTML += 
+//             `<a href="./product.html?id=${product._id}">
+//                 <article>
+//                 <img src="${product.imageUrl}" alt="${product.altTxt}">
+//                 <h3 class="productName">${product.name}</h3>
+//                 <p class="productDescription">${product.description}</article>
+//             </a>`;
+//       });
+//     });
+// }
+
+function getDataFromApi() {
+    fetch("http://localhost:3000/api/products")
+      .then((response) => response.json())
+      .then((data) => {
+        //for (let i = 0; i < data.length; i++) {
+        //for(product of data) {
+        data.map((product) => {
+          //createProduct(data, i);
+          document.querySelector(".items").innerHTML += `
+              <a href=./product.html?id=${product._id}>
+                  <article>
+                    <img src=${product.imageUrl} alt=${product.altTxt}>
+                    <h3 class="productName">${product.name}</h3>
+                    <p class="productDescription">${product.description}
+                  </article>
+              </a>`;
+        });
+      });
+  }
+
+ getDataFromApi();
