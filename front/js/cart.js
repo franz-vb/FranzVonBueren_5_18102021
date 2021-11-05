69aa5997bcacc3a6418a8a49ae1b5368cc0b12d7
franz von büren <fz.v.bueren@gmail.com>
2021-11-05 09:46:43 +0100

    add friday

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,29 @@
 let cart;
+
+function displayProductWithSameId(index1, index2, product) {
+  document.querySelector("#cart__items").innerHTML += `
+  <article class="cart__item" data-id="${product._id}">
+    <div class="cart__item__img">
+      <img src=${product.imageUrl} alt=${product.altTxt}>
+    </div>
+    <div class="cart__item__content">
+      <div class="cart__item__content__titlePrice">
+        <h2>${product.name}</h2>
+        <p>${cart[index1]}</p>
+        <p>${product.price * cart[index2]} €</p>
+      </div>
+      <div class="cart__item__content__settings">
+        <div class="cart__item__content__settings__quantity">
+          <p>Qté : ${cart[index2]}</p>
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${cart[index2]}>
+        </div>
+        <div class="cart__item__content__settings__delete">
+          <p class="deleteItem">Supprimer</p>
+        </div>
+      </div>
+    </div>
+  </article>`
+}
  
 function addCartFromProduct() {
     fetch("http://localhost:3000/api/products")
@@ -9,33 +34,19 @@ function addCartFromProduct() {
           if (localStorage.getItem(`${product._id}`)) {
 
             cart = JSON.parse(localStorage.getItem(`${product._id}`));
-console.log(cart);
-            document.querySelector("#cart__items").innerHTML += `
-            <article class="cart__item" data-id="${product._id}">
-            <div class="cart__item__img">
-              <img src=${product.imageUrl} alt=${product.altTxt}>
-            </div>
-            <div class="cart__item__content">
-              <div class="cart__item__content__titlePrice">
-                <h2>${product.name}</h2>
-                <p>${cart[0]}</p>
-                <p>${product.price * cart[1]} €</p>
-              </div>
-              <div class="cart__item__content__settings">
-                <div class="cart__item__content__settings__quantity">
-                  <p>Qté : ${cart[1]}</p>
-                  <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${cart[1]}>
-                </div>
-                <div class="cart__item__content__settings__delete">
-                  <p class="deleteItem">Supprimer</p>
-                </div>
-              </div>
-            </div>
-          </article>`;
-          }
 
-        });
-      });
-  }
+            let nbrElements = cart.length / 2;
+            let length = 0;
+
+            for (let i = 0; i < nbrElements; i++) {
+              
+              displayProductWithSameId(length, length + 1, product);
+              length += 2;
+            }
+
+          }
+      })
+    })
+}
 
   addCartFromProduct();
--- front/js/product.js
+++ front/js/product.js
@@ -1,7 +1,8 @@
 const urlParams = new URLSearchParams(window.location.search); 
 const idProduct = urlParams.get("id");
-const btnCart = document.querySelector("#addToCart");
+const btnPanier = document.querySelector("#addToCart");
 const itemQty = document.querySelector('#quantity');
+let isColorPresent = true;
 
 let myProduct; 
 let productTitle = document.querySelector("#title");
@@ -11,44 +12,70 @@ let productImg = document.querySelector(".item__img");
 let productColorsOptions = document.querySelector('#colors');
 
 function getProductFromId() {
-    fetch("http://localhost:3000/api/products/"+ idProduct)
+    fetch("http://localhost:3000/api/products/" + idProduct )
     .then(response => response.json())
     .then(product => { 
 
         productTitle.textContent = `${product.name}`;
         productDescription.textContent = `${product.description}`
         productPrice.textContent = `${product.price}`;
-        productImg.innerHTML += `<img src=${product.imageUrl} alt="${product.altTxt}"/>`;
+        productImg.innerHTML += `<img src=${product.imageUrl} alt=Photographie d'un canapé />`;
 
-            
-        //for (let i = 0; i < product.colors.length; i++) {
-            product.colors.map((color) => {  
+        product.colors.map((color) => {
             productColorsOptions.innerHTML += `<option value="${color}">${color}</option>`;
-            })
-
+        })
     });
 
 };
 
-btnCart.addEventListener("click", () => {
+btnPanier.addEventListener("click", () => {
 
-    let cart;
+    //Stocker le localStorage dans le tableau cart
+    let cart = JSON.parse(localStorage.getItem(`${idProduct}`));
 
+    //On vérifie si dans le localStorage se trouve la clé "idProduct"
     if (localStorage.getItem(`${idProduct}`)) {
 
-        cart = JSON.parse(localStorage.getItem(`${idProduct}`));
-        cart[1] = parseInt(cart[1]);
 
-        if (cart[0] == productColorsOptions.value) {
+        //Si oui alors on va boucler afin de vérifier les éléments pairs qui sont censés être la couleur
+        for (let i = 0; i < cart.length; i = i + 2) {
 
-            cart[1] += parseInt(itemQty.value);
-            localStorage.setItem(`${idProduct}`, JSON.stringify(cart));
+            //Si la couleur qu'on a sélectionné (productColorsOptions.value) est égale à celle présente dans l'Item du localStorage, alors : 
+            if (cart[i] == productColorsOptions.value) {
+                //On modifier la quantité de cette couleur
+                //Et on balance la variable booléenne isColorPresent à true
+                isColorPresent = true;    
+                cart[i + 1] += parseInt(itemQty.value);
+                localStorage.setItem(`${idProduct}`, JSON.stringify(cart));
+                break;
+            }
+
+            //Si la couleur sélectionnée n'est pas la même que la couleur de l'Item, alors on passe le booléen isColorPresent à false
+            //On utilise ici un booléen car si dans le else on push des nouveaux éléments dans le tableau cart, on va créer une boucle infini car mon FOR est dépendant 
+            //d'une longueur et cette longueur est modifiée à chaque fois.
+            else {
+                isColorPresent = false;
+            }
         }
-        
+
+        //Si le booléen isColorPresent est à False, cela veut dire qu'on a séléctionnée un produit déjà présent dans le local mais qui a une couleur différente
+
+        if (!isColorPresent) {
+
+            //Donc on ajoute deux nouveaux éléments au tableau cart avec la nouvelle couleur et la nouvelle quantité
+            //On push dans le tableau la nouvelle couleur
+            cart.push(productColorsOptions.value);
+            //On push dans le tableau la nouvelle quantité
+            cart.push(parseInt(itemQty.value));
+            //On met à jour le localStorage en pushant le nouveau tableau qui contient les nouveaux éléments
+            localStorage.setItem(`${idProduct}`, JSON.stringify(cart));
+
+        }  
     }
 
     else {
-        localStorage.setItem(`${idProduct}`, JSON.stringify([productColorsOptions.value, itemQty.value]));
+
+        localStorage.setItem(`${idProduct}`, JSON.stringify([productColorsOptions.value, parseInt(itemQty.value)]));
         alert("Produit ajouté au panier");
     }
 
