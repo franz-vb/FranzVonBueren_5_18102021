c8208fa62c653d58ac59f159afdd0f114e1ab12d
franz von büren <fz.v.bueren@gmail.com>
2021-11-17 10:53:17 +0100

    add new

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,81 +1,35 @@
-let cart;
+let cart = JSON.parse(localStorage.getItem("panier"));
+//console.log(cart);
 
-function handleOnChange() {
-  const inputQty = document.getElementsByClassName("itemQuantity");
-  const qtyInput = document.getElementsByClassName("qty");
-  const priceProduct = document.getElementsByClassName("priceProduct");
-  const nameProduct = document.getElementsByClassName("nameProduct");
-
-  for (let i = 0; i < inputQty.length; i++) {
-
-    inputQty[i].addEventListener('change', (e) => {
-
-      if (!localStorage.getItem(`${nameProduct[i].textContent}`)) {
-        localStorage.setItem(`${nameProduct[i].textContent}`, JSON.stringify(parseInt(priceProduct[i].textContent)));
-      }
-
-      qtyInput[i].textContent = `Qté : ${e.target.value}`;
-      priceProduct[i].textContent =  localStorage.getItem(`${nameProduct[i].textContent}`) * e.target.value;
-      
-    })
-  }
-}
-
-function displayProductWithSameId(index1, index2, product) {
-
-  document.querySelector("#cart__items").innerHTML += `
-  <article class="cart__item" data-id="${product._id}">
-    <div class="cart__item__img">
-      <img src=${product.imageUrl} alt=${product.altTxt}>
-    </div>
-    <div class="cart__item__content">
-      <div class="cart__item__content__titlePrice">
-        <h2 class="nameProduct">${product.name}</h2>
-        <p>${cart[index1]}</p>
-        <p class="priceProduct">${product.price * cart[index2]} €</p>
-      </div>
-      <div class="cart__item__content__settings">
-        <div class="cart__item__content__settings__quantity">
-          <p class="qty">Qté : ${cart[index2]}</p>
-          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${cart[index2]}>
-        </div>
-        <div class="cart__item__content__settings__delete">
-          <p class="deleteItem">Supprimer</p>
-        </div>
-      </div>
-    </div>
-  </article>`
-
-}
- 
-function addCartFromProduct() {
-    fetch("http://localhost:3000/api/products")
-      .then((response) => response.json())
-      .then((data) => {
-        data.map((product) => {
-
-          if (localStorage.getItem(`${product._id}`)) {
-
-            cart = JSON.parse(localStorage.getItem(`${product._id}`));
-
-            let nbrElements = cart.length / 2;
-            let length = 0;
-
-            for (let i = 0; i < nbrElements; i++) {
-              
-              displayProductWithSameId(length, length + 1, product);
-              length += 2;
-            }
-
-          }
-          
-        })
-      })
-      
-      setTimeout(function() {
-         handleOnChange(); 
-        }, 1000);
-
-  }
+function getProductFromCart() {
     
-  addCartFromProduct();
+            cart.forEach((product) => {
+                console.log(product)
+                document.querySelector("#cart__items").innerHTML += `
+                    <article class="cart__item" data-id="${product.id}">
+                        <div class="cart__item__img">
+                        <img src="${product.img}" alt="Photographie d'un canapé">
+                        </div>
+                        <div class="cart__item__content">
+                        <div class="cart__item__content__titlePrice">
+                            <h2>${product.name}</h2>
+                            <p>${product.color}</p>
+                            <p>${product.price}€</p>
+                        </div>
+                        <div class="cart__item__content__settings">
+                            <div class="cart__item__content__settings__quantity">
+                            <p>Qté : </p>
+                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
+                            </div>
+                            <div class="cart__item__content__settings__delete">
+                            <p class="deleteItem">Supprimer</p>
+                            </div>
+                        </div>
+                        </div>
+                    </article>`;
+            })
+            
+        };
+
+getProductFromCart();
+//console.log(cart);
--- front/js/product.js
+++ front/js/product.js
@@ -1,86 +1,127 @@
-const urlParams = new URLSearchParams(window.location.search); 
+/* const urlParams = new URLSearchParams(window.location.search); 
 const idProduct = urlParams.get("id");
-const btnPanier = document.querySelector("#addToCart");
-const itemQty = document.querySelector('#quantity');
-let isColorPresent = true;
 
-//let myProduct; 
-let productTitle = document.querySelector("#title");
-let productDescription = document.querySelector("#description");
-let productPrice = document.querySelector("#price");
-let productImg = document.querySelector(".item__img");
-let productColorsOptions = document.querySelector('#colors');
+let btnCart = document.querySelector("#addToCart");
+let quantity = document.querySelector("#quantity");
 
-function getProductFromId() {
-    fetch("http://localhost:3000/api/products/" + idProduct )
-    .then(response => response.json())
-    .then(product => { 
 
-        productTitle.textContent = `${product.name}`;
-        productDescription.textContent = `${product.description}`
-        productPrice.textContent = `${product.price}`;
-        productImg.innerHTML += `<img src=${product.imageUrl} alt=Photographie d'un canapé />`;
-
-        product.colors.forEach((color) => {
-            productColorsOptions.innerHTML += `<option value="${color}">${color}</option>`;
-        })
-    });
-
-};
-
-//Ajouter au panier.
-btnPanier.addEventListener("click", () => {
-
-    //Stocker le localStorage dans le tableau cart
-    let cart = JSON.parse(localStorage.getItem(`${idProduct}`));
-
-    //On vérifie si dans le localStorage se trouve la clé "idProduct"
-    if (localStorage.getItem(`${idProduct}`)) {
-        //console.log(cart);
-        //boucler afin de vérifier les éléments pairs = couleur.
-        //for (let i = 0; i < cart.length; i = i + 2) {
-            cart.map((element) => {
-                //console.log(element.color);
-                //couleur qu'on a sélectionnée est égale à celle présente dans l'Item du localStorage
-            if (element.color == productColorsOptions.value) {
-                isColorPresent = true; 
-                //On modifier la quantité de cette couleur 
-                //cart[i + 1] += parseInt(itemQty.value);  
-                //localStorage.setItem(`${idProduct}`, JSON.stringify(cart));
-                //break;
-            }
-
-            //Si la couleur sélectionnée n'est pas la même que la couleur de l'Item, alors on passe le booléen isColorPresent à false
-            //On utilise ici un booléen car si dans le else on push des nouveaux éléments dans le tableau cart, on va créer une boucle infini 
-            //car mon FOR est dépendant d'une longueur et cette longueur est modifiée à chaque fois.
-            else {
-                isColorPresent = false;
-            }
-            }) 
+function getProductFromApi() {
+    fetch("http://localhost:3000/api/products/" + idProduct)
+        .then((response) => response.json())
+        .then(product => {
+                document.querySelector(".item").innerHTML = `
+                <article>
+                <div class="item__img">
+                  <img src="${product.imageUrl}" alt="${product.altTxt}">
+                </div>
+                <div class="item__content">
     
-        //}
+                  <div class="item__content__titlePrice">
+                    <h1 id="title">${product.name}</h1>
+                    <p>Prix : <span id="price">${product.price}</span>€</p>
+                  </div>
+    
+                  <div class="item__content__description">
+                    <p class="item__content__description__title">Description :</p>
+                    <p id="description">${product.description}</p>
+                  </div>
+    
+                  <div class="item__content__settings">
+                    <div class="item__content__settings__color">
+                      <label for="color-select">Choisir une couleur :</label>
+                      <select name="color-select" id="colors">
+                          <option value="">--SVP, choisissez une couleur --</option>
+<!--                       <option value="vert">vert</option>
+                          <option value="blanc">blanc</option> -->
+                      </select>
+                    </div>
+    
+                    <div class="item__content__settings__quantity">
+                      <label for="itemQuantity">Nombre d'article(s) (1-100) :</label>
+                      <input type="number" name="itemQuantity" min="1" max="100" value="0" id="quantity">
+                    </div>
+                  </div>
+    
+                  <div class="item__content__addButton">
+                    <button id="addToCart">Ajouter au panier</button>
+                  </div>
+    
+                </div>
+              </article>`;
+              product.colors.forEach((colors) => {
+                document.querySelector("#colors").innerHTML +=
+                `<option value="${colors}">${colors}</option>`; 
+              })
+            })
+        }; */
 
-        //Si le booléen isColorPresent est à False, cela veut dire qu'on a séléctionnée un produit déjà présent dans le local mais qui a 
-        //une couleur différente.
-
-        if (!isColorPresent) {
-
-            //Donc on ajoute deux nouveaux éléments au tableau cart
-            cart.push(productColorsOptions.value);
-            cart.push(parseInt(itemQty.value));
-            localStorage.setItem(`${idProduct}`, JSON.stringify(cart));
-
-        }  
-    }
-
-    else {//Quand j'appuie sur panier, si idProduct n'existe pas dans le localStorage alors éxécute le code suivant
-        //Ajoute au localStorage un nouvel item qui contient une clé (idProduct) et une valeur (ici un tableau qui contient la couleur 
-        //sélectionnée ainsi que la quantité)
-
-        localStorage.setItem(`${idProduct}`, JSON.stringify([productColorsOptions.value, parseInt(itemQty.value)]));
-        alert("Produit ajouté au panier");
-    }
-
-})
-
-getProductFromId();
+        const urlParams = new URLSearchParams(window.location.search); 
+        const idProduct = urlParams.get("id");
+        const btnCart = document.querySelector("#addToCart");
+        const itemQty = document.querySelector('#quantity');
+                        
+        let productName = document.querySelector("#title");
+        let productDescription = document.querySelector("#description");
+        let productPrice = document.querySelector("#price");
+        let productImg = document.querySelector(".item__img");
+        let productColorsOptions = document.querySelector('#colors');
+        let quantity = document.querySelector("#quantity");
+        let cart ;
+        let isPresent = true;
+        
+        function getProductFromId() {
+          fetch("http://localhost:3000/api/products/" + idProduct )
+          .then(response => response.json())
+          .then(product => { 
+        
+              productName.textContent = `${product.name}`;
+              productDescription.textContent = `${product.description}`
+              productPrice.textContent = `${product.price}`;
+              productImg.innerHTML += `<img id=productImg src=${product.imageUrl} alt=Photographie d'un canapé />`;
+        
+              product.colors.forEach((color) => {
+                  productColorsOptions.innerHTML += `<option value="${color}">${color}</option>`;
+              })
+          });
+        };
+        
+        getProductFromId();
+                       
+        btnCart.addEventListener("click", (e) => {
+          
+          if (localStorage.getItem("panier")) {
+            cart = JSON.parse(localStorage.getItem("panier"));
+            console.log(cart);
+            cart.forEach((product) => {
+              //produits dans le panier
+              //console.log(product.name + product.color);
+              //nouveau produit sélectionné
+              //console.log(productName.textContent + productColorsOptions.value);
+              if (product.id + product.color == idProduct + productColorsOptions.value) {
+                //console.log("egal");
+                product.quantity += parseInt(quantity.value);
+                localStorage.setItem("panier", JSON.stringify(cart));
+                isPresent = true;
+              }
+              else {
+                isPresent = false;
+              }
+            });
+          } else {
+            cart = [];
+            cart.push({id: idProduct, quantity: parseInt(quantity.value), color: productColorsOptions.value, name: productName.textContent, price: productPrice.textContent, img: document.querySelector("#productImg").src});
+            localStorage.setItem("panier", JSON.stringify(cart));
+          }
+           if (!isPresent) {
+            cart.push({id: idProduct, quantity: parseInt(quantity.value), color: productColorsOptions.value, name: productName.textContent, price: productPrice.textContent, img: document.querySelector("#productImg").src});
+            localStorage.setItem("panier", JSON.stringify(cart));
+           } 
+        });
+        
+        
+        
+        
+        
+        
+            
+        
