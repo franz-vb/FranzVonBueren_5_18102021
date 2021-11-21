f057c30cfb32737d7b9361933ee9767c720c803b
franz von büren <fz.v.bueren@gmail.com>
2021-11-21 12:19:17 +0100

    add sunday

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,10 +1,43 @@
 let cart = JSON.parse(localStorage.getItem("panier"));
-//console.log(cart);
+let inputQuantity;
+// let newPrice = cart[index].price * cart[index].quantity;
+// let newPrice = document.querySelector("#totalPrice");
+// console.log(totalPrice);
+
+function onChangeInputsQuantity() {
+    inputQuantity = document.querySelectorAll(".itemQuantity");
+    inputQuantity.forEach((quantity) => {
+        quantity.addEventListener("change", (e) => {
+           let index = [...inputQuantity].indexOf(quantity);
+           //console.log([...inputQuantity].indexOf(quantity));
+           cart[index].quantity = parseInt(e.target.value); 
+           //console.log(cart[index].quantity );
+           //console.log(e.target.value);
+           let newPrice = cart[index].price * cart[index].quantity;
+           localStorage.setItem("panier", JSON.stringify(cart));
+           //console.log(newPrice);
+           
+           document.querySelector("#newPrice").textContent = `${newPrice}`;
+           //return newPrice;
+        })
+    });
+};
+
+// function updatePrice(price) {
+    
+//     //localStorage.getItem("panier", JSON.stringify(cart));
+//     console.log(cart);
+//     cart.forEach((product) => {
+        
+//     });
+
+// };
+
 
 function getProductFromCart() {
     
             cart.forEach((product) => {
-                console.log(product)
+                //console.log(product)
                 document.querySelector("#cart__items").innerHTML += `
                     <article class="cart__item" data-id="${product.id}">
                         <div class="cart__item__img">
@@ -14,22 +47,31 @@ function getProductFromCart() {
                         <div class="cart__item__content__titlePrice">
                             <h2>${product.name}</h2>
                             <p>${product.color}</p>
-                            <p>${product.price}€</p>
+                            <p id="newPrice">${product.price * product.quantity}€</p>
                         </div>
                         <div class="cart__item__content__settings">
                             <div class="cart__item__content__settings__quantity">
-                            <p>Qté : </p>
-                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
+                              <p>Qté : </p>
+                              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}"/>
                             </div>
                             <div class="cart__item__content__settings__delete">
-                            <p class="deleteItem">Supprimer</p>
+                              <p class="deleteItem">Supprimer</p>
                             </div>
                         </div>
                         </div>
                     </article>`;
+                    //document.querySelector("#totalPrice").textContent = `${product.price}`;
             })
             
+             
         };
 
 getProductFromCart();
-//console.log(cart);
+onChangeInputsQuantity();
+//updatePrice();
+
+
+
+
+
+
--- front/js/product.js
+++ front/js/product.js
@@ -1,59 +1,3 @@
-/* const urlParams = new URLSearchParams(window.location.search); 
-const idProduct = urlParams.get("id");
-
-let btnCart = document.querySelector("#addToCart");
-let quantity = document.querySelector("#quantity");
-
-
-function getProductFromApi() {
-    fetch("http://localhost:3000/api/products/" + idProduct)
-        .then((response) => response.json())
-        .then(product => {
-                document.querySelector(".item").innerHTML = `
-                <article>
-                <div class="item__img">
-                  <img src="${product.imageUrl}" alt="${product.altTxt}">
-                </div>
-                <div class="item__content">
-    
-                  <div class="item__content__titlePrice">
-                    <h1 id="title">${product.name}</h1>
-                    <p>Prix : <span id="price">${product.price}</span>€</p>
-                  </div>
-    
-                  <div class="item__content__description">
-                    <p class="item__content__description__title">Description :</p>
-                    <p id="description">${product.description}</p>
-                  </div>
-    
-                  <div class="item__content__settings">
-                    <div class="item__content__settings__color">
-                      <label for="color-select">Choisir une couleur :</label>
-                      <select name="color-select" id="colors">
-                          <option value="">--SVP, choisissez une couleur --</option>
-<!--                       <option value="vert">vert</option>
-                          <option value="blanc">blanc</option> -->
-                      </select>
-                    </div>
-    
-                    <div class="item__content__settings__quantity">
-                      <label for="itemQuantity">Nombre d'article(s) (1-100) :</label>
-                      <input type="number" name="itemQuantity" min="1" max="100" value="0" id="quantity">
-                    </div>
-                  </div>
-    
-                  <div class="item__content__addButton">
-                    <button id="addToCart">Ajouter au panier</button>
-                  </div>
-    
-                </div>
-              </article>`;
-              product.colors.forEach((colors) => {
-                document.querySelector("#colors").innerHTML +=
-                `<option value="${colors}">${colors}</option>`; 
-              })
-            })
-        }; */
 
         const urlParams = new URLSearchParams(window.location.search); 
         const idProduct = urlParams.get("id");
@@ -91,8 +35,8 @@ function getProductFromApi() {
           
           if (localStorage.getItem("panier")) {
             cart = JSON.parse(localStorage.getItem("panier"));
-            console.log(cart);
-            cart.forEach((product) => {
+            //console.log(cart);
+            cart.forEach((product) => { //product : valeur de l'élément(objet) du tableau.
               //produits dans le panier
               //console.log(product.name + product.color);
               //nouveau produit sélectionné
